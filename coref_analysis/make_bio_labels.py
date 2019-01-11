e58cc0bc2f19e5fe0c4b045bea78366517255595
Neha Nayak <nnayak@umass.edu>
2019-01-11 16:16:17 -0500

    make bio labels file actually

---
+++ coref_analysis/make_bio_labels.py
@@ -0,0 +1,96 @@
+import collections
+import sys
+import re
+
+
+label_splitter = re.compile("([0-9]+|\(|\))")
+
+
+class BIOLabels(object):
+  B = "B"
+  I = "I"
+  O = "O"
+
+
+class TokenLabel(object):
+  def __init__(self, bio, entity):
+    self.bio = bio
+    self.entity = entity
+
+
+def change_label(bio_label, token_label):
+  token_label.bio = bio_label
+  return token_label
+
+
+def format_label(next_coref_field, curr_entities):
+
+  # Entities whose spans end at this token
+  to_end = set()
+
+  # Split out parens and entity numbers
+  contents = re.findall(label_splitter, next_coref_field)
+
+  while contents:
+    elem = contents.pop(0)
+
+    if elem == "(":
+      # Should start a new entity span
+      num = contents.pop(0)
+      assert num.isdigit()
+      curr_entities.add(TokenLabel(BIOLabels.B, num))
+
+      if contents:
+        # Check for single-token mentions
+        maybe_close_brace = contents.pop(0)
+        if maybe_close_brace == ")":
+          to_end.add(num)
+        else:
+          contents.insert(0, maybe_close_brace)
+
+    elif elem != ")":
+      # Check for span ending
+      close_brace = contents.pop(0)
+      assert close_brace == ")"
+      to_end.add(elem)
+
+  final_labels = "|".join(sorted(x.bio + '-' + x.entity
+      for x in curr_entities))
+
+  curr_entities = set([change_label(BIOLabels.I, label)
+      for label in curr_entities
+      if label.entity not in to_end])
+
+  return curr_entities, final_labels
+
+
+def read_conll_file(conll_file):
+  """Convert coref labels to BIO and write to stdout."""
+  sentences = []
+  curr_entities = set()
+
+  with open(conll_file, 'r') as f:
+    for line in f:
+      if line.startswith("#"):
+        sys.stdout.write(line)
+      else:
+        fields = line.split()
+        if not len(fields):
+          sys.stdout.write(line)
+          # Start a new sentence
+          curr_entities = set()
+        else:
+          curr_entities, new_label = format_label(fields[-1], curr_entities)
+          output = "\t".join(fields[:-1] + [new_label])
+          sys.stdout.write(output + "\n")
+
+
+def main():
+
+  conll_file = sys.argv[1]
+  read_conll_file(conll_file)
+
+
+if __name__ == "__main__":
+  main()
+
